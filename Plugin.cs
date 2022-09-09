075eb1a4232ac56f9607241c3904e75e1c91f58e
Hydraxous <robo-trey@outlook.com>
2022-09-09 03:20:58 -0700

    General improvement
    Fixed
    - menu specification error
    
    Changes
    - optimization
    - added precursor code for upcoming feature

--- Plugin.cs
+++ Plugin.cs
@@ -6,92 +6,53 @@ using UnityEngine.SceneManagement;
 namespace EasyPZ
 {
 
-    [BepInPlugin("Hydraxous.ULTRAKILL.EasyPZ", "EasyPZ", "1.0.0")]
+    [BepInPlugin("Hydraxous.ULTRAKILL.EasyPZ", "EasyPZ", "1.0.1")]
     public class EasyPZ : BaseUnityPlugin
     {
+        private enum RestartType {Explosion, Instant}
+
         private OptionsManager om;
         private NewMovement player;
         private StatsManager sman;
 
         private ConfigEntry<KeyCode> PMODE_TOGGLE;
         private ConfigEntry<KeyCode> RESTART_MISSION;
+        private ConfigEntry<RestartType> RESTART_TYPE;
 
-        bool enablePMode = true;
-        bool pActive = false;
+        private GameObject Boom; //Unused
+
+        public bool PMode = true;
 
         private void Awake()
         {
             Logger.LogInfo("EasyPZ Loaded. Good luck on P-ing in all the levels! :D");
 
-            PMODE_TOGGLE = Config.Bind("General", "PMODE_TOGGLE", KeyCode.P, "Key to toggle P-Mode");
-            RESTART_MISSION = Config.Bind("General", "RESTART_MISSION", KeyCode.RightAlt, "Instantly restarts the mission");
-            FindThings();
-            
+            PMODE_TOGGLE = Config.Bind("Binds", "PMODE_TOGGLE", KeyCode.P, "Key to toggle P-Mode");
+            RESTART_MISSION = Config.Bind("Binds", "RESTART_MISSION", KeyCode.RightAlt, "Restarts the mission");
+            RESTART_TYPE = Config.Bind("General", "RESTART_TYPE", RestartType.Instant, "Explosion causes you to explode when you fail P-Rank, instant just restarts the mission without having to click respawn. NOTE: Explosion is not yet implemented and will just make you instantly die when pressing the key.");
+          
         }
         
         private void Update()
         {
             if(Input.GetKeyDown(PMODE_TOGGLE.Value))
             {
-                enablePMode = !enablePMode;
-            }
-            if(Input.GetKeyDown(RESTART_MISSION.Value))
-            {
-                QuickRestart();
+                PMode = !PMode;
             }
 
-            if(player.dead && enablePMode)
+            if(Input.GetKeyDown(RESTART_MISSION.Value))
             {
-                QuickRestart();
+                FailPRank(RESTART_TYPE.Value);
             }
         }
 
         private void LateUpdate()
         {
-            if (enablePMode)
+            if (PMode)
             {
                 FindThings();
+                if (player.dead) { FailPRank(RestartType.Instant); } //Checks if player died.
                 CheckTimeGoalFailed();
-            }
-        }
-
-        private void OnGUI()
-        {
-            
-            if (player != null)
-            {
-                pActive = player.activated;
-            }
-            if (MonoSingleton<OptionsManager>.Instance.paused && pActive)
-            {
-                GUI.Label(new Rect(new Vector2(Screen.width/2.1f,Screen.height/3.14f), new Vector2(Screen.width/10, Screen.height/10)), "P-Mode: " + enablePMode.ToString());
-            }
-        }
-
-        //Restarts level entirely
-        private void QuickRestart()
-        {
-            if (om != null && SceneManager.GetActiveScene().name != "Main Menu")
-            {
-                om.RestartMission();
-            }
-        }
-
-        //Restarts from last checkpoint
-        private void QuickCheckpoint()
-        {
-            if (om != null)
-            {
-                om.RestartCheckpoint();
-            }
-        }
-
-        //Kills the player via explosion... Quite funny.
-        private void KillPlayer()
-        {
-            if (player != null)
-            {
-                player.GetHurt(200, false, 1, true, false);
             }
         }
 
@@ -113,17 +74,68 @@ namespace EasyPZ
                 player = MonoSingleton<NewMovement>.Instance;
             }
         }
+
+        private void OnGUI()
+        {
+            
+            if (player != null)
+            {
+                if (MonoSingleton<OptionsManager>.Instance.paused && player.activated)
+                {
+                    GUI.Label(new Rect(new Vector2(Screen.width / 2.1f, Screen.height / 3.14f), new Vector2(Screen.width / 10, Screen.height / 10)), "P-Mode: " + PMode.ToString());
+                }
+            }
+        }
+
+        //Kills the player via explosion, sorta... Quite funny. Unused for the moment.
+        private void ExplodePlayer()
+        {
+            if (player != null && Boom != null)
+            {
+
+                GameObject newBoom = Instantiate<GameObject>(Boom, player.transform.position, Quaternion.identity);
+                foreach(Explosion explosion in newBoom.GetComponentsInChildren<Explosion>())
+                {
+                    explosion.sourceWeapon = MonoSingleton<GunControl>.Instance.currentWeapon;
+                    explosion.enemyDamageMultiplier = 99999f;
+                    explosion.maxSize *= 99f;
+                    explosion.damage = 99999;
+                }
+            }   
+        }
+
+        
         
         //Checks if time goal has been failed: A rank.
         private void CheckTimeGoalFailed()
         {
-            if (sman != null && enablePMode && SceneManager.GetActiveScene().name != "Main Menu")
+            if (sman != null)
             {
                 if (sman.GetRanks(sman.timeRanks, sman.seconds, true, false) == "<color=#FF6A00>A</color>")
                 {
-                    KillPlayer();
+                    FailPRank(RESTART_TYPE.Value);
                 }
             }
+        }
+
+
+        private void FailPRank(RestartType manType)
+        {
+            if (om != null && SceneManager.GetActiveScene().name != "Main Menu" && player != null)
+            {
+                switch (manType)
+                {
+                    case RestartType.Explosion:
+                        //ExplodePlayer();
+                        player.GetHurt(200, false, 1, false, true);
+                        break;
+                    default:
+                        om.RestartMission();
+                        break;
+                }
+                
+            }
+            
         }
     }
 }
