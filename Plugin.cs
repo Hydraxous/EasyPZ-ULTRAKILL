3989326dc21a2a2279ab7bf298cade7e54b1a318
Hydraxous <robo-trey@outlook.com>
2022-09-10 12:58:34 -0700

    GREATLY Improved UI
    I implemented a small UI to display the information useful to you. It looks nice now.

--- Plugin.cs
+++ Plugin.cs
@@ -1,27 +1,42 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using EasyPZ.Properties;
+
 
 namespace EasyPZ
 {
-
     [BepInPlugin("Hydraxous.ULTRAKILL.EasyPZ", "EasyPZ", "1.0.1")]
     public class EasyPZ : BaseUnityPlugin
     {
         private enum RestartType {Explosion, Instant}
+        private enum HUDInfoType {Simple, Default}
 
         private OptionsManager om;
         private NewMovement player;
         private StatsManager sman;
+        private LevelStats lStats;
 
         private ConfigEntry<KeyCode> PMODE_TOGGLE;
         private ConfigEntry<KeyCode> RESTART_MISSION;
         private ConfigEntry<RestartType> RESTART_TYPE;
+        private ConfigEntry<HUDInfoType> HUD_INFO_TYPE;
+        private ConfigEntry<bool> PMODE_DEFAULT_STATE;
+        private ConfigEntry<bool> ALWAYS_SHOW_TRACKER;
 
-        private GameObject Boom; //Unused
+        private GameObject PStatusIndicatorPrefab;
+        private static UIPositionData PSI_UIPData = new UIPositionData(Vector2.zero, new Vector2(100,100),Vector2.one,Vector2.one,Vector2.one);
+        private GameObject RankGoalIndicatorPrefab;
+        private static UIPositionData RGI_UIPData = new UIPositionData(new Vector2(-10, 10), new Vector2(150f, 202.5f), new Vector2(1,0), new Vector2(1, 0), new Vector2(1, 0));
 
-        public bool PMode = true;
+
+        public GameObject rankTrackerUIElement;
+
+        public static AssetBundle easyPZAssets;
+        public bool PMode = false;
+
 
         private void Awake()
         {
@@ -30,9 +45,13 @@ namespace EasyPZ
             PMODE_TOGGLE = Config.Bind("Binds", "PMODE_TOGGLE", KeyCode.P, "Key to toggle P-Mode");
             RESTART_MISSION = Config.Bind("Binds", "RESTART_MISSION", KeyCode.RightAlt, "Restarts the mission");
             RESTART_TYPE = Config.Bind("General", "RESTART_TYPE", RestartType.Instant, "Explosion causes you to explode when you fail P-Rank, instant just restarts the mission without having to click respawn. NOTE: Explosion is not yet implemented and will just make you instantly die when pressing the key.");
-          
+            HUD_INFO_TYPE = Config.Bind("General", "HUD_INFO_TYPE", HUDInfoType.Default, "Changes hud type. Simple is a tiny P icon in corner when game is paused. Default is the goal UI on the left hand side when stats are turned on.");
+            PMODE_DEFAULT_STATE = Config.Bind("General", "PMODE_DEFAULT_STATE", false, "Sets whether P-Mode should be enabled by default.");
+            ALWAYS_SHOW_TRACKER = Config.Bind("General", "ALWAYS_SHOW_TRACKER", false, "Shows tracker when stats are not open in level.");
+            LoadAssets();
         }
         
+
         private void Update()
         {
             if(Input.GetKeyDown(PMODE_TOGGLE.Value))
@@ -44,15 +63,25 @@ namespace EasyPZ
             {
                 FailPRank(RESTART_TYPE.Value);
             }
+
         }
+
 
         private void LateUpdate()
         {
-            if (PMode)
+            if (PMode || InLevel())
             {
                 FindThings();
                 if (player.dead) { FailPRank(RestartType.Instant); } //Checks if player died.
                 CheckTimeGoalFailed();
+            }
+
+            if (rankTrackerUIElement == null && player != null)
+            {
+                CreateTracker();
+            }else
+            {
+                UpdateTracker();
             }
         }
 
@@ -73,38 +102,28 @@ namespace EasyPZ
             {
                 player = MonoSingleton<NewMovement>.Instance;
             }
-        }
 
-        private void OnGUI()
-        {
-            
-            if (player != null)
+            if (lStats == null)
             {
-                if (MonoSingleton<OptionsManager>.Instance.paused && player.activated)
-                {
-                    GUI.Label(new Rect(new Vector2(Screen.width / 2.1f, Screen.height / 3.14f), new Vector2(Screen.width / 10, Screen.height / 10)), "P-Mode: " + PMode.ToString());
-                }
+                lStats = GameObject.FindObjectOfType<LevelStatsEnabler>().transform.GetChild(0).GetComponent<LevelStats>();
+            }
+
+            if (rankTrackerUIElement == null)
+            {
+                CreateTracker();
             }
         }
 
-        //Kills the player via explosion, sorta... Quite funny. Unused for the moment.
-        private void ExplodePlayer()
+        //Checks if in level in which the display should not be shown
+        private bool InLevel()
         {
-            if (player != null && Boom != null)
+            string sceneName = SceneManager.GetActiveScene().name;
+            if (sceneName == "Intro" || sceneName == "Main Menu" || sceneName == "Cybergrind" || sceneName == "Sandbox")
             {
-
-                GameObject newBoom = Instantiate<GameObject>(Boom, player.transform.position, Quaternion.identity);
-                foreach(Explosion explosion in newBoom.GetComponentsInChildren<Explosion>())
-                {
-                    explosion.sourceWeapon = MonoSingleton<GunControl>.Instance.currentWeapon;
-                    explosion.enemyDamageMultiplier = 99999f;
-                    explosion.maxSize *= 99f;
-                    explosion.damage = 99999;
-                }
-            }   
+                return false;
+            }
+            return true;
         }
-
-        
         
         //Checks if time goal has been failed: A rank.
         private void CheckTimeGoalFailed()
@@ -118,10 +137,10 @@ namespace EasyPZ
             }
         }
 
-
+        //Restarts the mission.
         private void FailPRank(RestartType manType)
         {
-            if (om != null && SceneManager.GetActiveScene().name != "Main Menu" && player != null)
+            if (om != null && InLevel() && player != null)
             {
                 switch (manType)
                 {
@@ -136,6 +155,95 @@ namespace EasyPZ
                 
             }
             
+        }
+        
+        //Tries to instantiate the tracker object.
+        public void CreateTracker()
+        {
+            if (!InLevel()) { return; }
+
+            switch (HUD_INFO_TYPE.Value)
+            {
+                case HUDInfoType.Default:
+                    InstantiateTracker(RankGoalIndicatorPrefab, RGI_UIPData);
+                    break;
+                case HUDInfoType.Simple:
+                    InstantiateTracker(PStatusIndicatorPrefab, PSI_UIPData);
+                    break;
+            }
+        }
+
+
+        private void InstantiateTracker(GameObject prefab, UIPositionData uiData)
+        {
+            RectTransform canvas = MonoSingleton<CanvasController>.Instance.gameObject.GetComponent<RectTransform>();
+            rankTrackerUIElement = GameObject.Instantiate<GameObject>(prefab, canvas);
+            UIAutoPositioner positioner = rankTrackerUIElement.AddComponent<UIAutoPositioner>();
+            if (rankTrackerUIElement.name.Contains("RankGoal"))
+            {
+                rankTrackerUIElement.AddComponent<RankGoalTracker>();
+            }
+
+            positioner.uIPositionData = uiData;
+            positioner.CheckPosition();
+            UpdateTracker();
+        }
+        
+        //Updates the data in the tracker.
+        private void UpdateTracker()
+        {
+            switch(HUD_INFO_TYPE.Value)
+            {
+                case HUDInfoType.Default:
+                    if ((lStats.gameObject.activeInHierarchy || ALWAYS_SHOW_TRACKER.Value) && InLevel())
+                    {
+                        rankTrackerUIElement.SetActive(true);   
+                    }
+                    else
+                    {
+                        rankTrackerUIElement.SetActive(false);
+                    }
+                    break;
+                case HUDInfoType.Simple:
+                    if (om.paused)
+                    {
+                        rankTrackerUIElement.SetActive(true);
+                        if(PMode) //Sets color of indicator
+                        {
+                            rankTrackerUIElement.transform.GetChild(0).GetComponent<Text>().color = new Color(255,119,0,255);
+                        }else
+                        {
+                            rankTrackerUIElement.transform.GetChild(0).GetComponent<Text>().color = new Color(191, 191, 191, 150);
+                        }
+                    }
+                    else
+                    {
+                        rankTrackerUIElement.SetActive(false);
+                    }
+                    break;
+            }
+            
+        }
+
+        private void GetReverseCounter()
+        {
+
+        }
+
+        private void LoadAssets()
+        {
+            try
+            {
+                easyPZAssets = AssetBundle.LoadFromMemory(EasyPZResources.EasyPZ);
+                PStatusIndicatorPrefab = easyPZAssets.LoadAsset<GameObject>("PStatusIndicatorMini");
+                RankGoalIndicatorPrefab = easyPZAssets.LoadAsset<GameObject>("RankGoalIndicator");
+            }
+            catch (System.Exception e)
+            {
+                Logger.LogError(e.Message);
+                Logger.LogError("Could not load assets.");
+            }
+
         }
     }
 }
---
+++ UIAutoPositioner.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasyPZ
+{
+    public class UIAutoPositioner : MonoBehaviour
+    {
+        public UIPositionData uIPositionData;
+        private RectTransform rTransform;
+
+        private void Start()
+        {
+            rTransform = GetComponent<RectTransform>();
+            CheckPosition();
+        }
+
+        public void SetPositionData(UIPositionData data)
+        {
+            uIPositionData = data;
+        }
+
+        public void CheckPosition()
+        {
+            if (rTransform != null || uIPositionData != null)
+            {
+                if (!ValidatePosition())
+                {
+                    FixPosition();
+                }
+            }
+        }
+
+        public void FixPosition()
+        {
+            rTransform.anchorMin = uIPositionData.anchorMin;
+            rTransform.anchorMax = uIPositionData.anchorMax;
+            rTransform.anchoredPosition = uIPositionData.aPos;
+            rTransform.sizeDelta = uIPositionData.size;
+            rTransform.pivot = uIPositionData.pivot;
+        }
+
+        private bool ValidatePosition()
+        {
+
+            if (
+                rTransform.anchoredPosition != uIPositionData.aPos ||
+                rTransform.anchorMax != uIPositionData.anchorMax ||
+                rTransform.anchorMin != uIPositionData.anchorMin ||
+                rTransform.pivot != uIPositionData.pivot ||
+                rTransform.sizeDelta != uIPositionData.size
+                )
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void OnEnable()
+        {
+            CheckPosition();
+        }
+
+        private void OnDisable()
+        {
+            CheckPosition();
+        }
+    }
+
+    public class UIPositionData
+    {
+        public Vector2 aPos, size, anchorMin, anchorMax, pivot;
+
+        public UIPositionData(Vector2 aPos, Vector2 size, Vector2 anchorMin, Vector2 anchorMax, Vector2 pivot)
+        {
+            this.aPos = aPos;
+            this.size = size;
+            this.anchorMin = anchorMin;
+            this.anchorMax = anchorMax;
+            this.pivot = pivot;
+        }
+    }
+
+}
