5f9ae9dd0c50f4870db3da8f40cbd0c9d724dce4
Hydraxous <robo-trey@outlook.com>
2023-12-30 11:04:27 -0800

    Update ConstInfo.cs

--- ConstInfo.cs
+++ ConstInfo.cs
@@ -10,6 +10,7 @@ namespace EasyPZ
         public const string GUID = "Hydraxous.ULTRAKILL.EasyPZ";
         public const string VERSION = "3.0.0";
         public const string GITHUB_URL = "https://www.github.com/Hydraxous/EasyPZ-ULTRAKILL";
+        public const string GITHUB_VERSION_URL = "https://api.github.com/repos/Hydraxous/EasyPZ-ULTRAKILL/tags";
         public const string KOFI_URL = "https://www.ko-fi.com/Hydraxous";
         public const string DISCORD_URL = "https://discord.gg/kCHnwMDPt4";
     }
